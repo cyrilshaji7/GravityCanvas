800a6627c75597a47f074d94a7f83fd4c150d394
cyril shaji <cyrilshaji7@gmail.com>
2023-11-09 02:16:09 -0500

    added feature to destroy upon impact

--- n-body-sim.py
+++ n-body-sim.py
@@ -5,6 +5,7 @@ import numpy as np
 BODIES = 50
 ANIMATE_TIME = 1
 MIN = 1e-6
+MIN_DISTANCE = 0.2  # Adjust the value as needed
 
 
 class Vec2:
@@ -38,13 +39,16 @@ class Body:
 
 
 
+
 class Simulation:
     DT = 0.01
 
     def __init__(self, n):
         random.seed(time.time())
         self.bodies = []
+        self.bodies_destroyed = 0
         for _ in range(n):
+            # creating random positions, velocities and masses
             a = random.random() * 2 * math.pi
             sin_a = math.sin(a)
             cos_a = math.cos(a)
@@ -52,38 +56,45 @@ class Simulation:
             r = abs(r)
             pos = Vec2(cos_a, sin_a) * (n ** 0.5) * 10.0 * r
             vel = Vec2(sin_a, -cos_a)
-            mass = random.randint(1, 1000)
+            mass = 30
             body = Body(pos, vel, mass)
             self.bodies.append(body)
 
-        self.bodies.sort(key=lambda body: body.pos.x ** 2 + body.pos.y ** 2)
         for i in range(n):
+            # finding length/magnitude of vec2
             mag = math.sqrt(self.bodies[i].pos.x ** 2 + self.bodies[i].pos.y ** 2)
             v = (i / mag) - math.sqrt(2.0)
 
             self.bodies[i].vel = Vec2(self.bodies[i].vel.x * v, self.bodies[i].vel.y * v)
-
+    
     def update(self):
-        for i in range(len(self.bodies)):
-            p1 = self.bodies[i].pos
-            for j in range(i, len(self.bodies)):
-                if i != j:
-                    p2 = self.bodies[j].pos
-                    m2 = self.bodies[j].mass
+        bodies_to_destroy = set()  # Keep track of bodies to destroy
+        for i, body1 in enumerate(self.bodies):
+            for j, body2 in enumerate(self.bodies):
+                if i != j and j not in bodies_to_destroy:
+                    p1 = body1.pos
+                    p2 = body2.pos
+                    m2 = body2.mass
 
                     r = Vec2(p2.x - p1.x, p2.y - p1.y)
                     r_norm = r.get_length()
-                    # print(r)
                     mag_sq = r.x ** 2 + r.y ** 2
                     mag = math.sqrt(mag_sq)
                     a1 = r * (m2 / (max(mag_sq, MIN) * mag)) 
-                    self.bodies[i].acc.x += a1.x
-                    self.bodies[i].acc.y += a1.y
+                    body1.acc.x += a1.x
+                    body1.acc.y += a1.y
 
-            
+                    # Check if bodies are too close and mark them for destruction
+                    if mag < MIN_DISTANCE:
+                        bodies_to_destroy.add(i)
+                        bodies_to_destroy.add(j)
+                        self.bodies_destroyed += 1
 
-        for i in range(len(self.bodies)):
-            self.bodies[i].update(self.DT)
+        # Destroy marked bodies
+        self.bodies = [body for i, body in enumerate(self.bodies) if i not in bodies_to_destroy]
+
+        for body in self.bodies:
+            body.update(self.DT)
 
 
 # code to display animation using tkinter
@@ -106,6 +117,7 @@ screen_height = root.winfo_screenheight()
 num_of_bodies = simpledialog.askinteger("Input", "Number of bodies: ")
 
 sim = Simulation(num_of_bodies)
+
 canvas = tk.Canvas(root, width=screen_width, height=screen_height)
 canvas.pack()
 
@@ -118,20 +130,29 @@ def update():
     my_queue.append(min(1 / time_end, 999))
     real_time_fps = statistics.mean(data=my_queue)
 
-    text = f"FPS: {real_time_fps:.1f}\n n = {num_of_bodies}"
+    text = f"FPS: {real_time_fps:.1f}\n Bodies Start = {num_of_bodies} \n Bodies Left = {num_of_bodies - sim.bodies_destroyed}"
 
     canvas.delete("all") 
     canvas.create_text(screen_width/2, 10, text=text, fill="black", anchor="n")
     for body in sim.bodies:
         x, y = body.pos.x * 20 + screen_width/2, body.pos.y * 20 + screen_height/2  # Scale and shift for display
-        canvas.create_oval(x - 3, y - 3, x + 3, y + 3, fill="red")
+        canvas.create_oval(x - 7, y - 7, x + 7, y + 7, fill="red", )
+    # for body in sim.bodies:
+    #     x, y = body.pos.x * 20 + screen_width/2, body.pos.y * 20 + screen_height/2  # Scale and shift for display
+    #     # Calculate acceleration magnitude
+    #     acc_magnitude = 0
+    #     # Map acceleration magnitude to a color (e.g., from blue to red)
+    #     color_value = int(255 * min(acc_magnitude / 10.0, 1.0))
+    #     color = "#{:02X}00{:02X}".format(255 - color_value, color_value)
+        
+    #     canvas.create_oval(x - 7, y - 7, x + 7, y + 7, fill=color)
+
 
 def animate():
     update()
     root.after(ANIMATE_TIME, animate)  
     
 animate()
-
 root.mainloop()
 
 
